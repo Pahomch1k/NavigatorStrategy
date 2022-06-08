a8914e749531fcfc1ebc6ea0fdf57799c3ccf102
pavel <pavel@DESKTOP-EDGG59N>
2022-06-08 18:04:47 +0300

    Add project files.

---
+++ Dz-18_NavigatorStrategy/Navigator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz_18_NavigatorStrategy
+{
+    class Navigator
+    { 
+        private IStrategy _strategy;
+
+        public Navigator() { }
+         
+        public Navigator(IStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+         
+        public void SetStrategy(IStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+         
+        public void ChoiseMarsh()
+        {
+            Console.WriteLine(_strategy.Marshrut()) ;
+        }
+    }
+}
---
+++ Dz-18_NavigatorStrategy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace Dz_18_NavigatorStrategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var navigator = new Navigator();
+
+            int flag = 0;
+            while (flag == 0)
+            {
+                WriteLine("1. Road\n2. Walk\n3. Transport\n4. Exit");
+                int choise = Convert.ToInt32(ReadLine());
+
+                switch (choise)
+                {
+                    case 1: navigator.SetStrategy(new RoadStrat()); break;
+                    case 2: navigator.SetStrategy(new WalkingStrat()); break;
+                    case 3: navigator.SetStrategy(new TransportStrat()); break;
+                    default: flag++; break;
+                }
+                navigator.ChoiseMarsh();
+            } 
+        }
+    }
+}
